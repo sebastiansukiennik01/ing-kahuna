6fee02c24c791837b9ba00f730378540a61c434a
SebastiannSukiennik <sebastian.sukiennik@tradersclub24.de>
2024-04-03 17:46:39 +0200

    Add filling missing data in Active_mortgages and logging

--- processing.py
+++ processing.py
@@ -1,5 +1,10 @@
 import os
 import pandas as pd
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger("processing")
 
 
 def load_data(filename: str):
@@ -10,14 +15,21 @@ def load_data(filename: str):
     """
     filepath = os.path.join("data", filename) 
     
-    return pd.read_csv(filepath)
+    data = pd.read_csv(filepath)
+    
+    data = change_to_dates(data)
+    data = add_custom_variables(data)
+    data = drop_unnecessary_columns(data)
+    data = fill_missing_values(data)
+    
+    return data
 
 
 def change_to_dates(data: pd.DataFrame, columns: list = ["Ref_month",  "Birth_date", "Oldest_account_date", "Contract_origination_date", "Contract_end_date"]):
     """
     changes to dates
     """
-    # data[columns] = pd.to_datetime(data[columns])
+    log.info("Changing dates to datetime..")
     data['Ref_month'] = pd.to_datetime(data['Ref_month'], format='%m-%Y')
     data['Birth_date'] = pd.to_datetime(data['Birth_date'], format='%d-%m-%Y')
     data['Oldest_account_date'] = pd.to_datetime(data['Oldest_account_date'], format='%d-%m-%Y')
@@ -33,9 +45,32 @@ def add_custom_variables(data):
     args:
         data : pandas dataframe
     """
+    log.info("Adding custom variables to dataset..")
     # contract origin date + contract end data ===> months_left
     data["months_left"] = (data["Contract_end_date"] - data["Contract_origination_date"]) / pd.Timedelta(days=30)
     
     return data
+
+
+def drop_unnecessary_columns(data, columns: list = []):
+    """
+    Drops provided columns (lack of significance or data) AND deault columns.
+    args:
+        data : pandas dataframe
+        columns : columns names to be dropped
+    """ 
+    log.info("Dropping unnecesssary columns..")
+    deafault = ["External_credit_card_balance", "External_term_loan_balance", "External_mortgage_balance"]
     
+    return data.drop(columns=deafault + columns)
+    
+    
+def fill_missing_values(data):
+    """
+    Fill invalid data with more descriptive/appropriate values.
+    """
+    log.info("Filling missing values..")
+    data["Active_mortgages"] = data["Active_mortgages"].replace({-9999: 0})
+    
+    return data
     
--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-from processing import load_data, change_to_dates, add_custom_variables
+from processing import *
 
 
 if __name__ == "__main__":
     train = load_data("in_time.csv")
-    print(train.select_dtypes(include=['object']).head())
-
-    train = change_to_dates(train)
-    train = add_custom_variables(train)
-    # print(train.info())
-    print(train["months_left"])
+    print(train.info())
